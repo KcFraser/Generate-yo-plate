f2ecc059345f5b0ed98fdb0a32fe7bec0ca09eef
elbandingo <lewis.adam.r@gmail.com>
2021-11-26 22:33:18 -0330

    got the fetch working based on ingredient

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 var searchText = document.querySelector("#search-input");
 var searchBtn = document.querySelector("#search-btn");
 
+
+//add an event listener to capture the text from the input form
 searchBtn.addEventListener("click", function(){
-    var ingredientInput = searchText.value.trim();
-    console.log(ingredientInput);
+    var searchInput = searchText.value.trim();
+    console.log(searchInput);
+    //check to see if user entered anything, if not alert them to enter an ingredient
+    if (searchInput) {
+        getMeals(searchInput);
+    } else {
+        alert("Please enter an ingredient!");
+    }
+});
+
+//fetch the recipes form the API based on main ingredient
+var getMeals = function(userInput) {
+var urlApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + userInput;
+fetch(urlApi).then(function(response){
+    //check to see if the response was good, if not, alert that theres no meals found
+    if(response.ok) {
+        response.json().then(function(data){
+            //set the variables for the content
+            console.log(data);
+        })
+    } else {
+        alert("No Meals Found with this ingredient");
+    }
 })
+}
