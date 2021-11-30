47ab9009f4f51b7cef84d8b55c0e4d57fccb1f1e
elbandingo <lewis.adam.r@gmail.com>
2021-11-30 10:05:02 -0330

    got dynamic creation done, it assigns a unique ID to each recipie fetch button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var searchText = document.querySelector("#search-input");
 var searchBtn = document.querySelector("#search-btn");
 var recipeBtn = document.querySelector("#recipe-btn");
 var mealCreate = document.querySelector("#results-display");
-var mealsArr = [];
+
 
 
 //add an event listener to capture the text from the input form
@@ -31,20 +31,24 @@ fetch(urlApi).then(function(response){
 
             //if there are results, append the html
             if(data.meals) {
+                var i = 0;
                 data.meals.forEach(meal => {
+                    console.log(i);
+                    i++;
                     console.log(meal.strMeal);
-                    mealCreate.innerHTML += "<div class='w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button id='recipe-btn' class='bg-red-500 text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
-                    
+                    mealCreate.innerHTML += "<div class='w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button id='recipe-btn-" + i +"' class='bg-red-500 text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
                 });
+            } else {
+                alert("There is no meals for that ingredient!");
             }
-
+            //save the HTML content to local storage
             localStorage.setItem('searchResults', mealCreate.innerHTML);
 });
 
     }
 });
 
-//save the items to local storage
+
 
 }
 
