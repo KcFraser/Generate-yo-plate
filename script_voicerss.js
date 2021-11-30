1c158b55c0a6d202936c00e392fa97aa275fc719
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-30 18:54:22 -0500

    Re-committing files before merge

--- script_voicerss.js
+++ script_voicerss.js
@@ -4,18 +4,57 @@
 // Documentation at http://www.voicerss.org/api/
 
 // Get the div that contains the text to play as speech
-var instructions = document.querySelector("#instructions");
-
+//var instructions = document.querySelector("#instructions");
+var instructions = "Hello world";
 // Generate the API URL by entering the needed parameters
-var apiURL = "http://api.voicerss.org/?key=7814e85b6cf347d58749c22162e3d4e7&hl=en-gb&src=" + instructions.textContent;
+var apiURL = "http://api.voicerss.org/?key=7814e85b6cf347d58749c22162e3d4e7&hl=en-gb&src=" + instructions;
 
 // Create an "Audio" object and feed the API URL into it
 // Documentation on Audio object: https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement
-var myAudio = new Audio(apiURL);
+
+function playAudioFetchVer () {
+    fetch(apiURL)
+        .then(function(response) {
+            console.log(response.url);
+            return response.url;
+        })
+        .then(function(data) {
+	        console.log(data);
+            document.querySelector("#player").setAttribute("src",data);
+            //var myAudio = new Audio(data);
+            //console.dir(myAudio);
+            //myAudio.play();
+        })
+        .catch(function(error){
+            alert("error");
+        });
+}
+
 
 // Generate a function to play the audio and add an event listener to a button to play that function
 var playAudio = function() {
+    
+    // Audio plays on safari if it's already downloaded
+    //var myAudio = new Audio("test.wav");
+    
+    var myAudio = new Audio(apiURL);
+    console.log(apiURL);
+    console.dir(myAudio);
     myAudio.play();
 }
+
+var playAudioV2 = function() {
+    
+    // Audio plays on safari if it's already downloaded
+    //var myAudio = new Audio("test.wav");
+    
+    var myAudio = new Audio(apiURL);
+    console.log(apiURL);
+    var promise = myAudio.play();
+
+    if (promise !== undefined) {
+        promise.then(() => {}).catch(error => console.error);
+    }
+}
 var playBtn = document.querySelector("#play");
-playBtn.addEventListener("click", playAudio);
+playBtn.addEventListener("click", playAudioFetchVer);
