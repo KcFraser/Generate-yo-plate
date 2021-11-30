b9ac7e245be9c0d262afb04ec6567a3fd9ed1b6f
elbandingo <lewis.adam.r@gmail.com>
2021-11-29 23:50:53 -0330

    dynamic display done

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var searchText = document.querySelector("#search-input");
 var searchBtn = document.querySelector("#search-btn");
+var recipeBtn = document.querySelector("#recipe-btn");
+var mealCreate = document.querySelector("#results-display");
 var mealsArr = [];
 
 
@@ -22,20 +24,19 @@ fetch(urlApi).then(function(response){
     //check to see if the response was good, if not, alert that theres no meals found
     if(response.ok) {
         response.json().then(function(data){
-            //set the variables for the content, first check if any meals. if not, return out of function
-            if(!data.meals) {
-                alert("no meals found");
-                return;
-            } else {
-               //display the HTML results formatted in cards learn the HTML from this vid: https://www.youtube.com/watch?v=opikz5x_1ak&t=358s
-               //loop should go through first all found meals, displaying each one.;
-               for(var i = 0; i < data.meals.length; i++) {
-                    console.log(data.meals[i]);
-                    //dynamically create the card content here!!!
-               }
-            }
 
-        })
-    } 
-})
+            //create the elements for appending
+
+            //if there are results, append the html
+            if(data.meals) {
+                data.meals.forEach(meal => {
+                    console.log(meal.strMeal);
+                    mealCreate.innerHTML += "<div class='w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button id='recipe-btn' class='bg-red-500 text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
+                    
+                });
+            }
+});
+
+    }
+});
 }
