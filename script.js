71a704cd6b8f2edae7816fb4ff9f6a4acf9bb39b
Aragsan <aragsan.elmi@gmail.com>
2021-12-01 15:31:42 -0500

    updated html/script on a git pull error

--- script.js
+++ script.js
@@ -19,68 +19,39 @@ searchBtn.addEventListener("click", function() {
 
 //fetch the recipes form the API based on main ingredient
 var getMeals = function(userInput) {
-<<<<<<< HEAD
     var urlApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + userInput;
     fetch(urlApi).then(function(response) {
         //check to see if the response was good, if not, alert that theres no meals found
         if (response.ok) {
             response.json().then(function(data) {
 
-                //create the elements for appending
+                    //create the elements for appending
 
-                //if there are results, append the html
-                if (data.meals) {
-                    var i = 0;
-                    data.meals.forEach(meal => {
-                        console.log(i);
-                        i++;
-                        console.log(meal.strMeal);
-                        mealCreate.innerHTML += "<div class='w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button id='recipe-btn-" + i + "' class='text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
-                    });
-                } else {
-                    alert("There is no meals for that ingredient!");
+                    //if there are results, append the html
+                    if (data.meals) {
+                        var i = 0;
+                        data.meals.forEach(meal => {
+                            console.log(i);
+                            i++;
+                            console.log(meal.strMeal);
+
+                            // Get recipe/meal ID No.
+                            console.log("meal ID", meal.idMeal);
+
+                            mealCreate.innerHTML += "<div class='meals w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button data-meal_id='" + meal.idMeal + "' id='recipe-btn-" + i + "' class='bg-red-500 text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
+
+                        });
+                    } else {
+                        alert("There is no meals for that ingredient!");
+                    }
+                    //save the HTML content to local storage
+                    localStorage.setItem('searchResults', mealCreate.innerHTML);
                 }
-                //save the HTML content to local storage
-                localStorage.setItem('searchResults', mealCreate.innerHTML);
-            });
+
+            );
 
         }
     });
-=======
-var urlApi = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=' + userInput;
-fetch(urlApi).then(function(response){
-    //check to see if the response was good, if not, alert that theres no meals found
-    if(response.ok) {
-        response.json().then(function(data){    
-
-            //create the elements for appending
-
-            //if there are results, append the html
-            if(data.meals) {
-                var i = 0;
-                data.meals.forEach(meal => {
-                    console.log(i);
-                    i++;
-                    console.log(meal.strMeal);
-
-                    // Get recipe/meal ID No.
-                    console.log("meal ID",meal.idMeal);
-
-                    mealCreate.innerHTML += "<div class='meals w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button data-meal_id='" + meal.idMeal + "' id='recipe-btn-" + i +"' class='bg-red-500 text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
-                
-                });
-            } else {
-                alert("There is no meals for that ingredient!");
-            }
-            //save the HTML content to local storage
-            localStorage.setItem('searchResults', mealCreate.innerHTML);
-        }
-
-        );
-
-    }
-});
->>>>>>> 04082974039f623bb5dc8efbd0bcc1e7ed42eb17
 
 }
 
@@ -90,11 +61,8 @@ var loadStorage = function() {
 }
 
 // Runs when you click on the "Get Recipe" button
-var loadMealDetails = function(event){
+var loadMealDetails = function(event) {
 
-<<<<<<< HEAD
-//load any local storage content thus far on load
-=======
     // Find the "Get Recipe" button that was clicked. Then get the Meal ID number that's in it
     var mealID = event.target;
     mealID = mealID.getAttribute("data-meal_id");
@@ -102,10 +70,10 @@ var loadMealDetails = function(event){
     //Fetch the meal details
     var mealIDAPI = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealID;
     fetch(mealIDAPI)
-        .then(function(response){
+        .then(function(response) {
             return response.json();
         })
-        .then(function(data){
+        .then(function(data) {
 
             // Load the meal instructions into the div #instructions
             var instructionsElem = document.querySelector("#instructions");
@@ -114,7 +82,7 @@ var loadMealDetails = function(event){
             //Run the text to speech api
             var apiURL = "http://api.voicerss.org/?key=7814e85b6cf347d58749c22162e3d4e7&hl=en-gb&src=" + data.meals[0].
             strInstructions;
-            console.log('api url',apiURL);
+            console.log('api url', apiURL);
             var myAudio = new Audio(apiURL);
             myAudio.play();
 
@@ -128,4 +96,3 @@ mealList.addEventListener("click", loadMealDetails);
 
 //load any local storage content thus far on load
 loadStorage();
->>>>>>> 04082974039f623bb5dc8efbd0bcc1e7ed42eb17
