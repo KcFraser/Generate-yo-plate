04082974039f623bb5dc8efbd0bcc1e7ed42eb17
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-11-30 21:44:27 -0500

    Removed redundancies in the script

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ var searchBtn = document.querySelector("#search-btn");
 var recipeBtn = document.querySelector("#recipe-btn");
 var mealCreate = document.querySelector("#results-display");
 
-
-
 //add an event listener to capture the text from the input form
 searchBtn.addEventListener("click", function(){
     //clear out any old meals
@@ -42,7 +40,6 @@ fetch(urlApi).then(function(response){
 
                     mealCreate.innerHTML += "<div class='meals w-full overflow-hidden sm:my-4 sm:px-4 sm:w-1/2 md:my-1 md:px-1 md:w-full lg:my-6 lg:px-6 lg:w-1/3 xl:my-6 xl:px-6 xl:w-1/3 border border-solid bg-gray-200 rounded-lg ...'><img class='w-full' src=" + meal.strMealThumb + " alt='Mountain'><div class='px-6 py-4'><div class='font-bold text-xl mb-2' id='search-result-0'>" + meal.strMeal + "</div><div class='px-6 pt-4 pb-2 flex justify-end'><button data-meal_id='" + meal.idMeal + "' id='recipe-btn-" + i +"' class='bg-red-500 text-white font-bold uppercase w-50 text-sm px-6 py-3 rounded-full shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 items-center' type='button'>Get Recipe</button></div>";
                 
-                    
                 });
             } else {
                 alert("There is no meals for that ingredient!");
@@ -56,8 +53,6 @@ fetch(urlApi).then(function(response){
     }
 });
 
-
-
 }
 
 var loadStorage = function() {
@@ -65,7 +60,12 @@ var loadStorage = function() {
     mealCreate.innerHTML = loadResults;
 }
 
-var loadMealDetails = function(mealID){
+// Runs when you click on the "Get Recipe" button
+var loadMealDetails = function(event){
+
+    // Find the "Get Recipe" button that was clicked. Then get the Meal ID number that's in it
+    var mealID = event.target;
+    mealID = mealID.getAttribute("data-meal_id");
 
     //Fetch the meal details
     var mealIDAPI = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealID;
@@ -74,9 +74,8 @@ var loadMealDetails = function(mealID){
             return response.json();
         })
         .then(function(data){
-            console.log("Recipe",data);
-            console.log(data.meals[0].strMeal);
 
+            // Load the meal instructions into the div #instructions
             var instructionsElem = document.querySelector("#instructions");
             instructionsElem.textContent = data.meals[0].strInstructions;
 
@@ -88,25 +87,12 @@ var loadMealDetails = function(mealID){
             myAudio.play();
 
         })
-        /* .then(function () {
-
-            
-            
-        }); */
 
 }
 
-var getMealDetails = function(event){
-    //console.log(event.target);
-    var mealID = event.target;
-    mealID = mealID.getAttribute("data-meal_id");
-    //console.log(mealID);
-    loadMealDetails(mealID);
-}
-
+// Add Event listener to #results-display so that "Get Recipe" buttons will be able to trigger the needed functions
 var mealList = document.querySelector("#results-display");
-mealList.addEventListener("click", getMealDetails);
-loadStorage();
+mealList.addEventListener("click", loadMealDetails);
 
 //load any local storage content thus far on load
-
+loadStorage();
