42dab51a7e1a09c7b180fa80804e667bec6caee7
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-12-02 19:13:57 -0500

    First attempted bugfix

--- script_text_to_speech.js
+++ script_text_to_speech.js
@@ -3,13 +3,33 @@
 // API provided by Voice RSS (voicerss.org)
 // Documentation at http://www.voicerss.org/api/
 
+var myAudio;
+
 // Documentation on Audio object: https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement
 
+// Discern what was clicked and run the appropriate function
+var processClick = function(event){
+    var modalID = event.target;
+    modalID = modalID.getAttribute("data-modal");
+    modalID = "#modal" + modalID;
+    console.log(modalID);
+
+    var btnClass = event.target;
+    btnClass = btnClass.getAttribute("class");
+
+    if (btnClass === "play") {
+        playAudio(modalID);
+    } else if (btnClass === "pause") {
+        stopAudio();
+    }
+}
+
 // Generate a function to play the audio and add an event listener to a button to play that function
-var playAudio = function() {
+var playAudio = function(modalID) {
 
     // Get the div that contains the text to play as speech
-    var instructions = document.querySelector(".instructions");
+    var instructionsDiv = modalID + " .instructions";
+    var instructions = document.querySelector(instructionsDiv);
     instructions = instructions.textContent;
     // Generate the API URL by entering the needed parameters
     var apiURL = "http://api.voicerss.org/?key=7814e85b6cf347d58749c22162e3d4e7&hl=en-gb&src=" + instructions;
@@ -19,8 +39,12 @@ var playAudio = function() {
             return response;
         })
         .then(function(data){
+
+            if(myAudio) {
+                myAudio.pause();
+            }
             // Create an "Audio" object and feed the API URL into it
-            var myAudio = new Audio(data.url);
+            myAudio = new Audio(data.url);
             myAudio.play();
         })
         .catch(function(error) {
@@ -28,5 +52,24 @@ var playAudio = function() {
             console.dir(error);
         })
 }
-var playBtn = document.querySelector("#play");
-playBtn.addEventListener("click", playAudio);
+
+// Stop the audio if it's playing
+var stopAudio = function(){
+    console.log("Audio stopped");
+    myAudio.pause();
+}
+
+
+// For #modal1
+var playBtn1 = document.querySelector("#modal1");
+playBtn1.addEventListener("click", processClick);
+
+var stopBtn1 = document.querySelector("#modal1");
+stopBtn1.addEventListener("click", stopAudio);
+
+// For #modal2
+var playBtn2 = document.querySelector("#modal2 .play");
+playBtn2.addEventListener("click", playAudio("#modal2"));
+
+var stopBtn2 = document.querySelector("#modal2 .stop");
+stopBtn2.addEventListener("click", stopAudio);
