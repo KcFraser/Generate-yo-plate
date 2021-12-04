4aac1ac78976ebd9c5277fc3c5b099116d1e3d65
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-12-03 19:13:50 -0500

    develop/daniel_bugfix_speech

--- script_text_to_speech.js
+++ script_text_to_speech.js
@@ -29,6 +29,7 @@ var playAudio = function(modalID) {
 
     // Get the div that contains the text to play as speech
     var instructionsDiv = modalID + " .instructions";
+    console.log(instructionsDiv);
     var instructions = document.querySelector(instructionsDiv);
     instructions = instructions.textContent;
     // Generate the API URL by entering the needed parameters
@@ -68,8 +69,8 @@ var stopBtn1 = document.querySelector("#modal1");
 stopBtn1.addEventListener("click", stopAudio);
 
 // For #modal2
-var playBtn2 = document.querySelector("#modal2 .play");
-playBtn2.addEventListener("click", playAudio("#modal2"));
+var playBtn2 = document.querySelector("#modal2");
+playBtn2.addEventListener("click", processClick);
 
-var stopBtn2 = document.querySelector("#modal2 .stop");
+var stopBtn2 = document.querySelector("#modal2");
 stopBtn2.addEventListener("click", stopAudio);
