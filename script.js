13e078564ba574b69d26fcdf8dedfdc67c9151ee
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-12-04 20:38:40 -0500

    Added 'scroll to top' button

--- script.js
+++ script.js
@@ -128,3 +128,20 @@ var mealList = document.querySelector("#results-display");
 mealList.addEventListener("click", loadMealDetails);
 //load any local storage content thus far on load
 loadStorage();
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
